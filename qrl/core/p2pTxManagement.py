925cdd69611bf6ae4078ec2f2996a6204d34e236
cyyber <kaushal.forex@gmail.com>
2018-02-01 01:00:09 +0530

    Added Lead Block Limit

--- qrl/core/p2pTxManagement.py
+++ qrl/core/p2pTxManagement.py
@@ -51,6 +51,11 @@ class P2PTxManagement(P2PBaseObserver):
                 logger.warning('TX pool size full, incoming tx dropped. mr hash: %s', bin2hstr(msg_hash))
                 return
 
+        if mr_data.type == qrllegacy_pb2.LegacyMessage.BK:
+            if mr_data.block_number > source.factory.chain_height() + config.user.forward_block_limit:
+                logger.debug('Skipping block #%s', mr_data.block_number)
+                return
+
         if source.factory.master_mr.contains(msg_hash, mr_data.type):
             return
 
